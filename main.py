0bcf4e0155b7c3482bd76ecf21a9c1e2aabe9559
Kieranmcm07 <kieransworkstuff@gmail.com>
2024-11-26 22:22:27 +0000

    Just added a background line for the clock to be on

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from tkinter import Tk
 from tkinter import Label
+import time
 
+root = Tk()
+root.title("Digital Clock")
+
+
+digi_clock = Label(root, font=("arial", 150), bg="gray",fg="black")
+digi_clock.pack()
+
+
+root.mainloop()
